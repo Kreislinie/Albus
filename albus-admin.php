b15a17584d4c7078f45d124f3cbb17d9a1a45436
Simon Mettler <admin@simonmettler.com>
2019-10-21 14:18:32 +0200

    display custom logo

--- albus-admin.php
+++ albus-admin.php
@@ -25,10 +25,11 @@ function load_custom_wp_admin_style() {
 
   wp_enqueue_script('albus_script', plugin_dir_url( __FILE__ ) . 'js/custom.min.js', [], '0.1.0', true);
 
-      // WordPress library
-      wp_enqueue_media();
+  // WordPress media library
+  wp_enqueue_media();
 
 }
+
 add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style'); // backend
 add_action('admin_bar_init', 'load_custom_wp_admin_style'); // frontend (admin bar)
 
@@ -47,7 +48,7 @@ add_filter('admin_footer_text', 'albus_footer_text');
 
 
 /* ------------------------------------------------------------
-  3.0 - Option
+  3.0 - Options
 ------------------------------------------------------------ */
 
 // Adds option page.
@@ -57,7 +58,7 @@ function albus_options_menu() {
 
 add_action( 'admin_menu', 'albus_options_menu' );
 
-
+// Initialize settings.
 function albus_options_settings_init() {
 
   register_setting( 'albus-options', 'albus_options' );
@@ -81,41 +82,44 @@ function albus_options_settings_init() {
 
 add_action( 'admin_init', 'albus_options_settings_init' );
 
-
+// Custom logo field setup.
 function albus_settings_field_logo_render() {
 
   $albus_options = get_option( 'albus_options' );
   
-  $default_image = plugins_url('img/albus-admin-default-logo.jpg', __FILE__);
-  $name="albus_custom_logo";
+  // Default image, used when no custom logo set. Not displayed on the admin menu.
+  $default_image = plugins_url( 'img/albus-admin-default-logo.jpg', __FILE__ );
 
-  if ( !empty( $albus_options[$name] ) ) {
 
-    $image_attributes = wp_get_attachment_image_src( $albus_options[$name] );
+  if ( !empty( $albus_options['albus_custom_logo'] ) ) {
+
+    $image_attributes = wp_get_attachment_image_src( $albus_options['albus_custom_logo'], 'full' );
     $src = $image_attributes[0];
-    $value = $albus_options[$name];
+    $value = $albus_options['albus_custom_logo'];
 
   } else {
-      
+    
+    // If no custom logo used.
     $src = $default_image;
     $value = '';
 
   }
+
   $text = __( 'Upload', 'albus-admin' );
 
   ?>
 
   <div class="upload">
-    <img data-src="<?php echo $default_image?>" src="<?php echo $src?>" width="176px" height="auto" />
+    <img data-src="<?php echo $default_image?>" src="<?php echo $src?>" width="190px" height="auto" />
 
     <div>
 
-      <input type="hidden" name="albus_options[<?php echo $name ?>]" id="albus_options[<?php echo $name ?>]" value="<?php echo $value ?>" />
-      <button type="submit" class="upload_image_button button"><?php echo $text ?></button>
+      <input type="hidden" name="albus_options[albus_custom_logo]" id="albus_options[albus_custom_logo]" value="<?php echo $value ?>" />
+      <button type="submit" class="upload_image_button button"><?php esc_html_e( 'Upload Logo', 'albus-admin' ) ?></button>
       
       <?php
-      if ( !empty( $albus_options[$name] ) ) {
-        echo '<button type="submit" class="remove_image_button button">Clear</button>';
+      if ( !empty( $albus_options['albus_custom_logo'] ) ) {
+        printf( '<button type="submit" class="remove_image_button button">Clear</button>', esc_html__( 'Clear', 'albus-admin') );
       }
       ?>
       
@@ -128,10 +132,10 @@ function albus_settings_field_logo_render() {
 }
 
 function albus_settings_section_callback() {
-  echo __( '(ToDo) Description', 'albus-admin' );
+  echo __( 'Upload your custom logo.', 'albus-admin' );
 }
 
-function albus_options_page(  ) {
+function albus_options_page() {
   ?>
 
   <div class="wrap">
@@ -154,7 +158,22 @@ function albus_options_page(  ) {
 
 
 /* ------------------------------------------------------------
-  4.0 - Update functionality
+  4.0 - Custom Logo
+------------------------------------------------------------ */
+
+// Hooks into admin menu and prints logo.
+function albus_display_custom_logo() {
+  $albus_options = get_option( 'albus_options' );
+
+  $custom_logo =  wp_get_attachment_image_src( $albus_options['albus_custom_logo'], 'full' );
+  printf( '<li id="aa-custom-logo" ><img src="%s"></li>', $custom_logo[0] );
+}
+
+add_action('adminmenu', 'albus_display_custom_logo');
+
+
+/* ------------------------------------------------------------
+  5.0 - Update functionality
 ------------------------------------------------------------ */
 
 require 'vendors/plugin-update-checker/plugin-update-checker.php';
